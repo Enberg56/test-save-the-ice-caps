dbfb79b8c9fc162b0a6e31a2b04d0e811f0bba8a
Enberg65 <emilnberg@gmail.com>
2022-09-20 14:17:46 +0200

    show/hide - now only showing relevant inputfields

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const details = document.querySelector('.container-details')
 const physicaldetails = document.querySelector('.physical-details')
+const digitaldetails = document.querySelector('.digital-details')
 const inputareas = document.querySelectorAll('input')
 
-function handleClick(event) {
-  const checkmark = event.target;
-  if (checkmark.dataset.type = 'digital'){
-    physicaldetails.classList.remove('hidden')
-  }
-  if (checkmark.dataset.type = 'physical'){
-    physicaldetails.classList.remove('hidden')
-  }
-  else{
-    details.classList.add('hidden');
+physicaldetails.classList.add('hidden')
+digitaldetails.classList.add('hidden')
 
-  }
-  console.log(checkmark.dataset.type)
+
+function handleClick(e) {
+  const newsletter_type = e.tartet.dataset.type
+
+  if (newsletter_type == 'physical'){
+    digitaldetails.classList.toggle('hidden');
 };
+  if (newsletter_type == 'digital'){
+    physicaldetails.classList.toggle('hidden');
+};
+}
 
 inputareas.forEach((input) =>
 input.addEventListener('click', handleClick)
-);
+)
