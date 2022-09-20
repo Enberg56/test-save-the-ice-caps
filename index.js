fe3ae47d4ac9ce9552c23ca0084b7a4bd2a0099e
Enberg65 <emilnberg@gmail.com>
2022-09-20 16:47:25 +0200

    Monthly or ontime payment adjusting a <p>

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const details = document.querySelector('.container-details')
 const physicaldetails = document.querySelector('.physical-details')
 const digitaldetails = document.querySelector('.digital-details')
+const donationtext = document.querySelector('.donation_text')
 const inputareas = document.querySelectorAll('input')
+let frequenzy = 'Monthly donation of '
 
 physicaldetails.classList.add('hidden')
 digitaldetails.classList.add('hidden')
 
 
 function handleClick(e) {
-  const newsletter_type = e.target.dataset.type
+  const newsletterType = e.target.dataset.type
+  const donationFrequency = e.target.name
+  let inputValue = document.querySelector('[name="donation_amount"]').value
 
-  if (newsletter_type == 'physical'){
+console.log(frequenzy)
+  if (newsletterType == 'physical'){
     digitaldetails.classList.toggle('hidden');
 };
-  if (newsletter_type == 'digital'){
+  if (newsletterType == 'digital'){
     physicaldetails.classList.toggle('hidden');
 };
+  if (donationFrequency == 'donation_frequency' ) {
+
+
+    if (e.target.id=='monthly_frequency') {
+       frequenzy ='Monthly donation of '
+      } else {
+       frequenzy ='One time donation of '
+    }
+
+  }
+  donationtext.innerHTML=frequenzy + inputValue
 }
 
 inputareas.forEach((input) =>
